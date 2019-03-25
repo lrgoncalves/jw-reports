0d1f32e7ef0efcef2cd1c564ee6d5a11d0008ed9
Leandro Rocha <leandro.rocha@bennu.tv>
2019-03-25 18:18:27 -0300

    refactor oi jornais

--- routes/api.php
+++ routes/api.php
@@ -15,19 +15,18 @@ use Illuminate\Http\Request;
 
 Route::group(['prefix' => 'timbancavirtual'], function () {
 
+    Route::group(['prefix' => 'commands'], function() {
+        Route::get('/magazine-feature', function(Request $request, $date = null){
+            Artisan::call('timbanca:magazine-features');
+            return '';
+        });
+    });
 });
 
 
 Route::group(['prefix' => 'oijornais'], function () {
     
     Route::group(['prefix' => 'commands'], function() {
-        Route::get('/customer/category-trends/import/{date?}', function(Request $request, $date = null){
-            if (!$date) {
-                $date = Carbon\Carbon::yesterday()->format('Y-m-d');
-            }
-            Artisan::call('oijornais:customer-category-trend', ['date' => $date]);
-            return (Artisan::output());
-        });
 
         Route::get('/trending-topics/import', function(Request $request, $date = null){
             Artisan::call('oijornais:trending-topics');
@@ -36,11 +35,6 @@ Route::group(['prefix' => 'oijornais'], function () {
     });
 
     Route::prefix('v1')->middleware('jwtbob')->group(function() {
-        Route::prefix('customer')->group(function () {
-            Route::get('/category-trends/{msisdn}', 'CustomerCategoryTrendController@categoryTrends')
-                ->where('msisdn', '[0-9]+');
-        });
-
         Route::get('/trending-topics/{productId?}', 'TrendingTopicsController@index')
             ->where('productId', '[0-9]+');
     });
